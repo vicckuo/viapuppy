b0efdafc5fd6f27098cfc240db5ff9abed4916c3
Vic Kuo <eun.hru@gmail.com>
2023-01-30 14:37:14 +0800

    fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
-import DefaultLayout from '@/components/layout/DefaultLayout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { Session } from 'next-auth';
+import Router from 'next/router';
+import nProgress from 'nprogress';
 
 interface Props {
   session?: Session | null;
 }
+
+nProgress.configure({
+  showSpinner: false,
+});
+Router.events.on('routeChangeStart', () => nProgress.start());
+Router.events.on('routeChangeComplete', () => nProgress.done());
+Router.events.on('routeChangeError', () => nProgress.done());
 
 export default function App({ Component, pageProps }: AppProps<Props>) {
   return (
