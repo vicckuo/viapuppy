2eb5bb3a038f260ca4094d38e3f351590b2ebc8f
Vic Kuo <eun.hru@gmail.com>
2023-01-30 14:16:13 +0800

    fixed

--- pages/admin/posts/create/index.tsx
+++ pages/admin/posts/create/index.tsx
@@ -1,19 +1,26 @@
-import Editor, { FinalPost } from '@/components/editor';
-import AdminLayout from '@/components/layout/AdminLayout';
-import { generateFormData } from '@/utils/helper';
 import axios from 'axios';
 import { NextPage } from 'next';
+import AdminLayout from '@/components/layout/AdminLayout';
+import Editor, { FinalPost } from '@/components/editor';
+import { generateFormData } from '@/utils/helper';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 interface Props {}
 
 const Create: NextPage<Props> = () => {
+  const [creating, setCreating] = useState(false);
+  const router = useRouter();
+
   const handleSubmit = async (post: FinalPost) => {
+    setCreating(true);
     try {
       // we have to generate FormData
       const formData = generateFormData(post);
 
       // submit our post
       const { data } = await axios.post('/api/posts', formData);
+      router.push('/admin/posts/update/' + data.post.slug);
       // console.log(data);
     } catch (error: any) {
       console.log(error.response.data);
@@ -22,7 +29,10 @@ const Create: NextPage<Props> = () => {
   return (
     <AdminLayout title='新增文章'>
       <div className='max-w-4xl mx-auto'>
-        <Editor onSubmit={handleSubmit} />
+        <Editor
+          onSubmit={handleSubmit}
+          busy={creating}
+        />
       </div>
     </AdminLayout>
   );
